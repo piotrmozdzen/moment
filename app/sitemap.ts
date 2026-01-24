6dc8bf29f7d846809a08387c748d27a222d8845f
piotrmozdzen <piotr.mozdzen@protonmail.com>
2026-01-24 18:58:32 +0100

    seo: add sitemap and robots.txt

---
+++ app/sitemap.ts
@@ -0,0 +1,21 @@
+import { MetadataRoute } from 'next'
+import { siteConfig } from '@/lib/site-config'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    const routes = [
+        '',
+        '/o-nas',
+        '/oferta',
+        '/baza-koktajli',
+        '/realizacje',
+        '/kontakt',
+        '/privacy-policy',
+    ].map((route) => ({
+        url: `${siteConfig.url}${route}`,
+        lastModified: new Date(),
+        changeFrequency: 'monthly' as const,
+        priority: route === '' ? 1 : 0.8,
+    }))
+
+    return routes
+}
