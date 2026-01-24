7cb9049307f35591cc742b61e4d78871fa3f83f0
piotrmozdzen <piotr.mozdzen@protonmail.com>
2026-01-24 21:49:36 +0100

    seo: add google site verification metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,6 +54,9 @@ export const metadata: Metadata = {
     icon: '/icon.png',
     apple: '/icon.png',
   },
+  verification: {
+    google: "oGeFrc-pTKJeN022wUhyGhIcRhOfBDQc5crsbxWknkY",
+  },
 }
 
 import { Toaster } from 'sonner'
